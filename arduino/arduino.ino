06c6fc66ae14cb49d6e108c92fff790d70d4f326
Ville Kuosmanen <ville@villekuosmanen.com>
2024-07-01 18:29:48 +0100

    changes

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,17 @@
-int x; 
+int fsrPins[] = {A0, A1, A2};
+int fsrValue = 0;
+
 void setup() { 
-  Serial.begin(115200); 
-  Serial.setTimeout(1); 
+  Serial.begin(115200);
 } 
 void loop() { 
-  while (!Serial.available()); 
-  x = Serial.readString().toInt(); 
-  Serial.print(x + 1); 
+  for (int i = 0; i < 3; i++) {
+    fsrValue = analogRead(fsrPins[i]);  // Read the analog value from each FSR
+    // Print the value in the format "FSR,i,value"
+    Serial.print("FSR,");
+    Serial.print(i);
+    Serial.print(",");
+    Serial.println(fsrValue);
+  }
+  delay(100);
 }
