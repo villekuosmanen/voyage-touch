c563e07456c91cd8da136c80e797a58693ac0c8e
Ville Kuosmanen <ville@villekuosmanen.com>
2024-07-06 20:33:56 +0100

    add piezo sensors

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,31 @@
 int fsrPins[] = {A0, A1, A2};
 int fsrValue = 0;
 
+int piezoPins[] = {A3, A4, A5};
+int piezoValue = 0;
+
 void setup() { 
   Serial.begin(115200);
 } 
-void loop() { 
+void loop() {
+  // force sensitive resistors 
   for (int i = 0; i < 3; i++) {
-    fsrValue = analogRead(fsrPins[i]);  // Read the analog value from each FSR
+    fsrValue = analogRead(fsrPins[i]);
     // Print the value in the format "FSR,i,value"
     Serial.print("FSR,");
     Serial.print(i);
     Serial.print(",");
     Serial.println(fsrValue);
   }
-  delay(100);
+
+  // piezoelectric sensors
+  for (int i = 0; i < 3; i++) {
+    piezoValue = analogRead(piezoPins[i]);
+    // Print the value in the format "PZ,i,value"
+    Serial.print("PZ,");
+    Serial.print(i);
+    Serial.print(",");
+    Serial.println(piezoValue);
+  }
+  delay(10);
 }
--- test.py
+++ test.py
@@ -19,7 +19,10 @@ arduino.readline()
 time.sleep(0.5)
 
 progress_bars = []
-progress_bars = [tqdm(total=1000, desc=f'FSR {i+1}', ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}') for i in range(NO_OF_SENSORS)]
+for i in range(NO_OF_SENSORS):
+    progress_bars.append(tqdm(total=1000, desc=f'FSR {i+1}', ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'))
+for i in range(NO_OF_SENSORS):
+    progress_bars.append(tqdm(total=100, desc=f'PZ {i+1}', ncols=100, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'))
 
 while True:
     if arduino.in_waiting > 0:
@@ -29,18 +32,29 @@ while True:
             print(f"invalid format received via serial: {line}\n")
             continue
         try:
-            if tokens[0] != "FSR":
+            if tokens[0] == "FSR":
+                sensor_id = int(tokens[1])
+
+                # Convert FSR value to an integer
+                fsr_value = int(tokens[2])
+                # Clamp the value between 0 and 1000
+                fsr_value = max(0, min(1000, fsr_value))
+                # Update the progress bar
+                progress_bars[sensor_id].n = fsr_value
+                progress_bars[sensor_id].refresh()
+            elif tokens[0] == "PZ":
+                sensor_id = int(tokens[1])
+
+                # Convert FSR value to an integer
+                fsr_value = int(tokens[2])
+                # Clamp the value between 0 and 1000
+                fsr_value = max(0, min(100, fsr_value))
+                # Update the progress bar
+                progress_bars[NO_OF_SENSORS + sensor_id].n = fsr_value
+                progress_bars[NO_OF_SENSORS + sensor_id].refresh()
+            else:
                 print(f"expected sensor type to be FSR, got {tokens[0]}\n")
                 continue
 
-            sensor_id = int(tokens[1])
-
-            # Convert FSR value to an integer
-            fsr_value = int(tokens[2])
-            # Clamp the value between 0 and 1000
-            fsr_value = max(0, min(1000, fsr_value))
-            # Update the progress bar
-            progress_bars[sensor_id].n = fsr_value
-            progress_bars[sensor_id].refresh()
         except ValueError:
             pass  # Ignore invalid values
